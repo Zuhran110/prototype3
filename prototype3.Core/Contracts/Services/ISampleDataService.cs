a7da647e7ce09e7798141c028428270242e22d2d
35305 Muhammad Yousaf <35305@students.riphah.edu.pk>
2024-07-03 10:52:47 +0400

    <prototype3.core fixes>

--- prototype3.Core/Contracts/Services/ISampleDataService.cs
+++
@@ -1,9 +0,0 @@
-﻿using prototype3.Core.Models;
-
-namespace prototype3.Core.Contracts.Services;
-
-// Remove this class once your pages/features are using your data.
-public interface ISampleDataService
-{
-    Task<IEnumerable<SampleOrder>> GetListDetailsDataAsync();
-}
