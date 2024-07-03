5d971bec266e4e59bf0256d7a5908c8f69c3a741
Zuhran110 <100785335+Zuhran110@users.noreply.github.com>
2024-07-03 08:32:24 +0400

    Add files via upload

---
+++ prototype3.Core/Data/UserRepository.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using prototype3.Core.Models;
+
+namespace prototype3.Core.Data;
+
+public class UserRepository
+{
+    private readonly List<User> _users = new List<User>
+    {
+        new User { Id = 1, Name = "admin", Password = "admin123", Role = "Admin" },
+        new User { Id = 2, Name = "user", Password = "user123", Role = "User" }
+    };
+
+    public IEnumerable<User> GetAllUsers()
+    {
+        return _users;
+    }
+
+    public User GetUser(string username, string password)
+    {
+        return _users.FirstOrDefault(u => u.Name == username && u.Password == password);
+    }
+}
---
+++ prototype3.Core/Models/LoginModel.cs
@@ -0,0 +1,19 @@
+﻿using prototype3.Core.Data;
+using System.Collections.Generic;
+
+namespace prototype3.Core.Models;
+
+public class LoginModel
+{
+    private readonly UserRepository _userRepository;
+
+    public LoginModel()
+    {
+        _userRepository = new UserRepository();
+    }
+
+    public User VerifyUser(string username, string password)
+    {
+        return _userRepository.GetUser(username, password);
+    }
+}
